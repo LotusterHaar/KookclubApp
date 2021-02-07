ab81035fc2b356e8627b8579d64f9dfc9a2c9b6e
LAPTOP-9I879AR9\lotus <lotus.ter.haar@topicus.nl>
2021-02-07 19:14:32 +0100

    console.error: “fontFamily ”Roboto_medium" is not a system font and has not been loaded through Font.loadAsync

--- App.js
+++ App.js
@@ -2,18 +2,18 @@ import React, {useEffect} from "react";
 import {Container, Text, Content} from 'native-base';
 import {StyleSheet} from "react-native";;
 import * as Font from "expo-font";
+import { Ionicons } from '@expo/vector-icons';
 import MenuCard from "./components/MenuCard";
 
 export default function App() {
     useEffect(() => {
-        const loadFont = async () =>
-            await Font.loadAsync({
-                Roboto: require("native-base/Fonts/Roboto.ttf"),
-                Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-            });
-        loadFont();
-    }, []);
-    return (<Content>
+        (async () => await Font.loadAsync({
+            Roboto: require('native-base/Fonts/Roboto.ttf'),
+            Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+            ...Ionicons.font,
+        }))();
+    }, [])
+    return  (<Content>
         <MenuCard/>
         <MenuCard/>
         <MenuCard/>
@@ -22,11 +22,3 @@ export default function App() {
     </Content>)
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
